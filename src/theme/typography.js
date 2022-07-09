f726ab927a73561fe5dbd0fa1f56fd08454e4b0e
James Gordo <hello@jamesgordo.com>
2022-07-09 13:13:11 +0800

    Update Typography size and letter spacing.

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -4,47 +4,61 @@ const pxToRem = (val) => `${val / 16}rem`;
 /**
  * MUI Typography
  * @see https://mui.com/material-ui/customization/typography/
+ * @see https://www.webdesignerdepot.com/2020/07/the-designers-guide-to-letter-spacing/
  */
 const typography = {
   fontFamily: "'Roboto', sans-serif",
   h1: {
-    fontSize: pxToRem(72),
+    fontSize: pxToRem(96),
+    letterSpacing: -1.5,
   },
   h2: {
-    fontSize: pxToRem(64),
+    fontSize: pxToRem(60),
+    letterSpacing: -0.5,
   },
   h3: {
-    fontSize: pxToRem(56),
+    fontSize: pxToRem(48),
+    letterSpacing: 0,
   },
   h4: {
-    fontSize: pxToRem(48),
+    fontSize: pxToRem(34),
+    letterSpacing: 0.25,
   },
   h5: {
-    fontSize: pxToRem(40),
+    fontSize: pxToRem(96),
+    letterSpacing: 0,
   },
   h6: {
-    fontSize: pxToRem(32),
+    fontSize: pxToRem(16),
+    letterSpacing: 0.15,
   },
   subtitle1: {
     fontSize: pxToRem(16),
+    letterSpacing: 0.15,
   },
   subtitle2: {
     fontSize: pxToRem(14),
+    letterSpacing: 0.1,
   },
   body1: {
     fontSize: pxToRem(16),
+    letterSpacing: 0.5,
   },
   body2: {
     fontSize: pxToRem(14),
+    letterSpacing: 0.25,
   },
   button: {
-    fontSize: pxToRem(14),
+    ffontSize: pxToRem(14),
+    letterSpacing: 1.25,
   },
   caption: {
-    fontSize: pxToRem(16),
+    fontSize: pxToRem(12),
+    letterSpacing: 0.4,
   },
   overline: {
-    fontSize: pxToRem(14),
+    fontSize: pxToRem(10),
+    letterSpacing: 1.5,
   },
 };
 
